4e5d2beca05a9273b09a61ed785d97d76e251f57
Joan Puig Lledó <joanpulle@gmail.com>
2021-04-24 21:52:36 +0200

    view home new design

--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import Header from '../../components/Header/Header.jsx'
+import Header from '../../components/Header/Header.jsx';
+import cabecera from '../../img/cabecera.jpeg';
 
 const Home =()=>{
     return(
         <div className="container-home">
-            <Header/>
+            <div className="header-image">
+                <Header/>
+                <img src={cabecera} alt=""></img>
+                
+            </div>
+            <div className="last-trips">
+                <div className="last-trips-description"></div>
+                <div className="last-trips-publications">
+                    <div className="last-trips-publications-1"></div>
+                    <div className="last-trips-publications-2"></div>
+                    <div className="last-trips-publications-3"></div>
+                    <div className="last-trips-publications-4"></div>
+                </div>
+
+            </div>
+            <div className="suggestions">
+                <div className="suggestions-descriptions"></div>
+                <div className="suggestions-trips">
+                    <div className="suggestions-trips-1"></div>
+                    <div className="suggestions-trips-2"></div>
+                    <div className="suggestions-trips-3"></div>
+                </div>
+            </div>
+
+            
         </div>
     )
 };
