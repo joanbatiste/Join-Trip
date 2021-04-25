d026c91e5019fceb4adab5fc70e087a1c4d381bb
Joan Puig Lledó <joanpulle@gmail.com>
2021-04-25 15:32:32 +0200

    header integrate to main image

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -15,15 +15,20 @@ const Header = () =>{
                     </a>
                 </div>
                 <div className="header-container-header-slogan">viajes y escapadas para compartir</div>
-                <div className="header-container-header-login">Inicia sesión</div>
-                <div className="header-container-header-register">Regístrate</div>
+                <div className="header-container-header-register">
+                    <button id="register">Regístrate</button>
+                    </div>
+                <div className="header-container-header-login">
+                    <button id="login">Inicia sesión</button>
+                    </div>
+                
             </div>
             <div className="header-container-links">
                 <div className="header-container-links-titles">
-                    <div className="header-container-links-titles home">inicio</div>
+                    {/* <div className="header-container-links-titles home">inicio</div>
                     <div className="header-container-links-titles travels">viajes</div>
                     <div className="header-container-links-titles activities">actividades</div>
-                    <div className="header-container-links-titles contact">contacto</div>
+                    <div className="header-container-links-titles contact">contacto</div> */}
                 </div>
                 
             </div>
