4ea2828d5fd33a0145612c9473553f93e126c2a3
Joan Puig Lledó <joanpulle@gmail.com>
2021-05-03 22:54:50 +0200

    creating trips whitout control errors from frontend

--- frontend/src/views/PostTrip/PostTrip.jsx
+++ frontend/src/views/PostTrip/PostTrip.jsx
@@ -2,9 +2,13 @@ import React,{ useState } from 'react'
 import cabecera_profile from '../../img/Hombre-paisaje-Panoramica.jpeg';
 import LoguedHeader from '../../components/LoguedHeader/LoguedHeader.jsx';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+// import chechError from '../../utiles/utiles';
+import axios from "axios";
 
 function PostTrip(props) {
-    console.log(props);
+    let history = useHistory();
+
     const [postTrip, setPostTrip] = useState({
         title: "",
         destination: "",
@@ -12,13 +16,49 @@ function PostTrip(props) {
         days: "",
         description:"",
         link: "",
-        userId: props.user.id
     });
 
     //Manejador de estado de la publicacion de un trip
     const handleStatePostTrip = (e) => {
         setPostTrip({ ...postTrip, [e.target.name]: e.target.value });
     };
+    //Estado de mensajes de error
+    // const [message, setMessage] = useState('');
+
+    //Funcion para enviar datos al back y crear el trip
+    const sendDataTrip = async()=>{
+        // setMessage('');
+        // let notValidated = chechError(user);
+        // setMessage(notValidated);
+
+        // if (notValidated) {
+        //     return;
+        // }
+        //Datos a enviar
+        let postTripData = {
+            title: postTrip.title,
+            destination: postTrip.destination,
+            date: postTrip.date,
+            days: postTrip.days,
+            description: postTrip.description,
+            link: postTrip.link,
+            userId: props.user.id
+        }
+
+        //Endpoint para crear trip
+        let endPoitCreateTrip = `http://127.0.0.1:8000/api/users/${props.user.id}/trips`;
+        let response = await axios.post(endPoitCreateTrip, postTripData,{headers: {authorization:`Bearer ${props.user.api_token}`}});
+        console.log("soy el response del trip", response);
+        if(!response.data){
+            alert('Lo sentimos. Tu viaje no se ha podido publicar')
+        }else{
+            alert('Tu viaje se ha publicado. Podrás verlo en tu perfil de usuario')
+            setTimeout(() => {
+                history.push('/profile')
+
+            }, 1000)
+        }
+    }
 
     return (
         <div className="container-post-trip">
@@ -28,21 +68,21 @@ function PostTrip(props) {
             </div>
             <div className="post-form-container">
                 <div className="post-form-container-content">
-                    <p className='post-form-container-content-title'>Mis datos de perfil</p>
+                    <p className='post-form-container-content-title'>Rellena los datos de tu viaje</p>
                     <label htmlFor="" className='post-form-container-content-label'>Título</label>
                     <input type='text' name='title' className='post-form-container-content-input' onChange={handleStatePostTrip}></input>
                     <label htmlFor="" className='post-form-container-content-label'>Destino</label>
-                    <input type='name' name='destination' className='post-form-container-content-input' onChange={handleStatePostTrip}></input>
+                    <input type='text' name='destination' className='post-form-container-content-input' onChange={handleStatePostTrip}></input>
                     <label htmlFor="" className='post-form-container-content-label'>Fecha</label>
                     <input type='date' name='date' className='post-form-container-content-input'  onChange={handleStatePostTrip}></input>
                     <label htmlFor="" className='post-form-container-content-label'>Días</label>
-                    <input type='name' name='days' className='post-form-container-content-input' onChange={handleStatePostTrip}></input>
-                    <label htmlFor="" className='post-form-container-content-label'>Descripción</label>
-                    <input type='name' name='description' className='post-form-container-content-input' onChange={handleStatePostTrip}></input>
+                    <input type='text' name='days' className='post-form-container-content-input' onChange={handleStatePostTrip}></input>
+                    <label htmlFor="" className='post-form-container-content-label-description' >Descripción</label>
+                    <input type='text' name='description' className='post-form-container-content-input' onChange={handleStatePostTrip}></input>
                     <label htmlFor="" className='post-form-container-content-label'>Links de interés</label>
-                    <input type='name'  name='link' className='post-form-container-content-input' onChange={handleStatePostTrip}></input>
+                    <input type='text'  name='link' className='post-form-container-content-input' onChange={handleStatePostTrip}></input>
                     {/* {message} */}
-                    {/* <input type="button" value="Publicar viaje" className='post-form-container-content-button' onClick={updateData} /> */}
+                    <input type="button" value="Publicar viaje" className='post-form-container-content-button' onClick={sendDataTrip} />
                 </div>
 
             </div>
