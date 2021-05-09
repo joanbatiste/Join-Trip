656e19337670abaae7b36c705165e4a65eff591b
Joan Puig Lledó <joanpulle@gmail.com>
2021-05-09 12:39:15 +0200

    design of unlogued home view

--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -4,27 +4,31 @@ import axios from 'axios';
 import LoguedHeader from '../../components/LoguedHeader/LoguedHeader.jsx';
 import cabecera from '../../img/cabecera.jpeg';
 import { connect } from 'react-redux';
-import {SAVING} from '../../redux/types/tripTypes';
+import { SAVING } from '../../redux/types/tripTypes';
 import TripCard from '../../components/TripCard/TripCard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import trip1 from '../../img/trip1.jpeg'
 import trip2 from '../../img/granada.jpg';
 import trip3 from '../../img/rioja.jpeg';
+import { faComments, faIdCard, faSearchLocation, faSuitcase } from '@fortawesome/free-solid-svg-icons';
+
 
 const Home = (props) => {
     //Funcion para traerse todos los trips de la BD
     const [trip, setTrips] = useState([]);
     //Guardamos los datos de viajes en redux
-    props.dispatch({type: SAVING, payload:trip});
-    
+    props.dispatch({ type: SAVING, payload: trip });
+
 
     const getTrips = async () => {
         let endPointTrips = "http://127.0.0.1:8000/api/trips";
         let tripsResponse = await axios.get(endPointTrips);
-        
+
         console.log(tripsResponse.data, "a ver si hago otro join");
-        setTrips (tripsResponse.data)
+        setTrips(tripsResponse.data)
     }
-    
+
     //USEEFFECTS
     useEffect(() => {
         getTrips();
@@ -41,23 +45,79 @@ const Home = (props) => {
 
                 </div>
                 <div className="last-trips">
-                    <div className="last-trips-description"></div>
-                    <div className="last-trips-publications">
-                        <div className="last-trips-publications-1"></div>
-                        <div className="last-trips-publications-2"></div>
-                        <div className="last-trips-publications-3"></div>
-                        <div className="last-trips-publications-4"></div>
+                    <div className="last-trips-instructions">
+                        <h2>¿Te gusta viajar?</h2>
+                        <h5>Si te encanta viajar y no te gusta hacerlo sol@ estás en el lugar indicado</h5>
+                        <h5>Encontrar compañer@s de viaje es mut fácil:</h5>
+
+                        <div className="last-trips-instructions-steps">
+                            <div className="last-trips-instructions-steps">
+                                <div className="step-icon">
+                                    <FontAwesomeIcon icon={faIdCard} />
+                                </div>
+                                <div className="step-description">
+                                    <h5>Regístrate en nuestro sitio e inicia sesión</h5>
+                                </div>
+                            </div>
+                            <div className="last-trips-instructions-steps">
+                                <div className="step-icon">
+                                    <FontAwesomeIcon icon={faSearchLocation} />
+                                </div>
+                                <div className="step-description">
+                                    <h5>Publica tu plan o encuentra el viaje que más te guste</h5>
+                                </div>
+                            </div>
+                            <div className="last-trips-instructions-steps">
+                                <div className="step-icon">
+                                    <FontAwesomeIcon icon={faComments} />
+                                </div>
+                                <div className="step-description">
+                                    <h5>Resuelve todas tus dudas preguntando al resto de viajeros</h5>
+                                </div>
+                            </div>
+                            <div className="last-trips-instructions-steps">
+                                <div className="step-icon">
+                                    <FontAwesomeIcon icon={faSuitcase} />
+                                </div>
+                                <div className="step-description">
+                                    <h5>¡Prepara la maleta y disfruta del viaje!</h5>
+                                </div>
+                            </div>
+
+                        </div>
+
                     </div>
 
                 </div>
                 <div className="suggestions">
-                    <div className="suggestions-descriptions"></div>
+                    <div className="suggestions-descriptions">
+                        <h2>Sugerencias del mes</h2>
+                        <p className="home-texts">¿Nos permites sugerencias?
+Te mostramos algunos de los destinos de nuestro país que seguro te sorprenderán.</p>
+                    </div>
                     <div className="suggestions-trips">
-                        <div className="suggestions-trips-1"></div>
-                        <div className="suggestions-trips-2"></div>
-                        <div className="suggestions-trips-3"></div>
+
+                        <div className="suggestions-trips-1">
+                            <div className="suggestions-trips-1-image">
+                                <img src={trip1} alt=""></img>
+                            </div>
+                            <div className="suggestions-trips-1-description"></div>
+                        </div>
+                        <div className="suggestions-trips-2">
+                            <div className="suggestions-trips-2-image">
+                                <img src={trip2} alt=""></img>
+                            </div>
+                            <div className="suggestions-trips-2-description"></div>
+                        </div>
+                        <div className="suggestions-trips-3">
+                            <div className="suggestions-trips-3-image">
+                                <img src={trip3} alt=""></img>
+                            </div>
+                            <div className="suggestions-trips-3-description"></div>
+                        </div>
                     </div>
                 </div>
+
 
 
             </div>
@@ -77,24 +137,21 @@ const Home = (props) => {
 Conoce todos los rincones de España y conecta con viajeros como tu. </p>
                     </div>
                     <div className="last-trips-publications">
-                        {trip.slice(0,5).map(mytrips => {
+                        {trip.slice(0, 5).map(mytrips => {
                             return (
                                 <TripCard
                                     title={mytrips.title}
                                     destination={mytrips.destination}
                                     description={mytrips.description}
-                                    date = {mytrips.date}
-                                    days = {mytrips.days}
+                                    date={mytrips.date}
+                                    days={mytrips.days}
                                     link={mytrips.link}
-                                    id = {mytrips.id}
-                                    username = {mytrips.username}
+                                    id={mytrips.id}
+                                    username={mytrips.username}
                                 />
                             )
                         })}
-                        {/* <div className="last-trips-publications-1"></div>
-                        <div className="last-trips-publications-2"></div>
-                        <div className="last-trips-publications-3"></div>
-                        <div className="last-trips-publications-4"></div> */}
+
                     </div>
 
                 </div>
